51f043cc37fc0534f87692a6155858f794e7f730
indrajith76 <indrajithgoswami@gmail.com>
2026-02-17 20:52:29 +0600

    fix a bug on tranding product card

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const trandingProductContainer = document.getElementById("trandingProduct-container");
+const productDetailsModal = document.getElementById("productDetailsModal");
+const productDetailsBox = document.getElementById("productDetailsDiv");
 
 function product() {
     fetch("https://fakestoreapi.com/products")
@@ -43,6 +45,7 @@ function trandingProductHandle(allProduct) {
 
                     <div class="flex gap-3">
                         <button
+                        onclick="productDetails(${p.id})"
                             class="flex items-center justify-center w-full gap-1 py-1 text-gray-600 transition border border-gray-300 rounded-xl hover:bg-gray-50">
                             <i class="fa-regular fa-eye text-sm"></i>
                             Details
@@ -62,6 +65,57 @@ function trandingProductHandle(allProduct) {
 }
 
 product()
+
+function productDetails(id) {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(res => res.json())
+        .then(data => displayProductDetailsHandler(data))
+}
+
+function displayProductDetailsHandler(data) {
+    productDetailsModal.classList.remove("hidden");
+    window.document.body.classList.add("overflow-hidden")
+    productDetailsBox.innerHTML = "";
+    productDetailsBox.innerHTML = `
+            <div class="p-4 bg-[#f0f2f5]">
+                <img src="${data.image}" class="object-contain h-auto lg:h-full min-w-48 mix-blend-multiply" />
+            </div>
+            <div class="flex flex-col justify-between py-5 pr-5"> 
+                <h3 class="text-lg lg:text-2xl font-medium text-gray-800">
+                    ${data.title}
+                </h3>
+                <div class="flex items-center justify-between my-5">
+                    <span class="px-3 py-1 text-xs font-semibold text-orange-600 bg-orange-100 rounded-full">
+                        ${data.category}
+                    </span>
+                    <div class="flex items-center gap-1 text-sm text-gray-500">
+                        <i class="fa-solid fa-star text-yellow-500"></i>
+                        <span>${data.rating.rate} (${data.rating.count})</span>
+                    </div>
+                </div>
+
+                <p class="text-xs lg:text-[16px]">${data.description}</p>
+
+                <div class="flex items-center justify-between mt-5">
+                    <p class="text-xl font-bold text-gray-900 font2nd">
+                       Price: $${data.price}
+                    </p>
+                    <button
+                        class="flex items-center justify-center gap-1 px-5 py-1 text-white transition bg-orange-600 rounded-xl hover:bg-orange-700">
+                        <i class="fa-solid fa-cart-arrow-down"></i>
+                        Add
+                    </button>
+                </div>
+            </div>            
+
+    <button onclick="closeModal()" class="absolute cursor-pointer -right-2 -top-2"><i class="fa-solid fa-circle-xmark"></i></button>
+    `;
+}
+
+function closeModal() {
+    productDetailsModal.classList.add("hidden");
+    document.body.classList.remove("overflow-hidden");
+}
 
 // dom manipulation for menu
 const menuItem = document.getElementById("menu-item");
